685b6b30290782969d305196120ee21d4006c11f
zzy0372 <18138128152@163.com>
2019-05-16 10:53:06 +0800

    注释 视图request参数 路由传递多个参数 render简写调用模板 解除静态文件硬编码

--- demo1/booktest/urls.py
+++ demo1/booktest/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import url
 from . import views
 
+"""
+解除静态文件硬编码
+1、给应用添加命名空间
+2、给路由添加名字
+3、在模板中使用格式   {% url '命名空间名：路由名' 参数1 参数2 %}
+"""
+
 app_name = 'booktest'
 
 urlpatterns = [
@@ -9,10 +16,5 @@ urlpatterns = [
 
     # url 参数 第一个代表正则表达式  第二个代表视图函数  第三个代表url名字
     url(r'^index/$',views.index,name="index"),
-    """
-    解除静态文件硬编码
-    1/给应用添加命名空间
-    2、给路由添加名字
-    3、在模板中使用格式   {% url '命名空间名：路由名' 参数1 参数2 %}
-    """
+
 ]
