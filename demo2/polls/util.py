59564f96a0018231ad75860ce7bad05dd92c8d36
zzy0372 <18138128152@163.com>
2019-05-21 11:46:50 +0800

    Django自带授权 from django.contrib.auth import authenticate,login,logout

--- demo2/polls/util.py
+++ demo2/polls/util.py
@@ -5,11 +5,19 @@ def checklogin(fun):
     def check(request,*args):
         # 在cookie去用户
         # un = request.COOKIES.get('username')
-
+        print(request.user,'++++++')
+        print(request.user.is_authenticated)
         # 在session中取
-        un = request.session.get("username")
-        if un:
-            return fun(request,*args)
+        # un = request.session.get("username")
+        # if un:
+        #     return fun(request,*args)
+        # else:
+        #     return redirect(reverse('polls:login'))
+
+
+        if request.user and request.user.is_authenticated:
+            return fun(request, *args)
         else:
             return redirect(reverse('polls:login'))
+
     return check
--- demo2/polls/views.py
+++ demo2/polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,redirect,reverse
 from django.http import HttpResponse,HttpResponseRedirect
-from .models import Question,Choice
+from .models import Question,Choice,MyUser
 from .util import checklogin
 from django.views.generic import View
+from django.contrib.auth import authenticate,login as lgi,logout as lgo
 
 
 # class Login(View):
@@ -22,28 +23,55 @@ def login(request):
     if request.method == "GET":
         return render(request, 'polls/login.html')
     else:
+        # 没有使用Django自带用户系统
+        # username = request.POST.get("username")
+        # if username == "zzy":
+        #     # 登录成功之后需要将用户相关cookie存储
+        #     res = redirect(reverse('polls:index'))
+        #     # 设置cookit完成登录
+        #     # res.set_cookie("username", username)
+        #     # 通过session完成登录
+        #     request.session["username"] = username
+        #     return res
+        # else:
+        #     return render(request, 'polls/login.html', {"error": "用户名错误"})
+
         username = request.POST.get("username")
-        if username == "zzy":
-            # 登录成功之后需要将用户相关cookie存储
-            res = redirect(reverse('polls:index'))
-            # 设置cookit完成登录
-            # res.set_cookie("username", username)
-            # 通过session完成登录
-            request.session["username"] = username
-            return res
+        pwd = request.POST.get("password")
+        # MyUser.objects.filter(username = username, p)
+        user = authenticate(request, username = username,password = pwd)
+        if user:
+            print(user)
+            lgi(request,user)
+            return redirect(reverse('polls:index'))
         else:
-            return render(request, 'polls/login.html', {"error": "用户名错误"})
+            return render(request, 'polls/login.html', {"error": "用户名或者密码错误"})
+
 
 def logout(request):
     res = redirect(reverse('polls:login'))
     # res.delete_cookie("username")
     # 清除session登录信息
-    request.session.flush()
+    # request.session.flush()
+
+    #  调用退出登录方法
+    lgo(request)
     return res
 
 
 def regist(request):
-    return HttpResponse("登录成功")
+    if request.method == "POST":
+        username = request.POST.get("username_regi")
+        pwd = request.POST.get("password_regi")
+        pwd2 = request.POST.get("password_regi_2")
+        error = None
+        if pwd != pwd2:
+            error = "密码不一致"
+            return render(request, 'polls/login.html', {"error": error})
+        else:
+            MyUser.objects.create_user(username= username, password=pwd, url = 'http://zzy0371.com')
+            return redirect(reverse('polls:login'))
+
 
 
 # class Index(View):
