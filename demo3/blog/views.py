86179e087c8661236a8e88fad1a1e59c19e5b5a6
zzy0372 <18138128152@163.com>
2019-05-22 15:14:50 +0800

    分页器

--- demo3/blog/views.py
+++ demo3/blog/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
+from .models import Article
+#  Paginator  Page
+from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    pagenum = request.GET.get("page")
+    pagenum = 1 if pagenum==None  else pagenum
+    # 得到所有文章
+    articles = Article.objects.all().order_by("-views")
+
+    paginator = Paginator(articles,1)
+    # 传入页码得到一个页面   page包含所有信息
+    page = paginator.get_page(pagenum)
+
+    return render(request,'index.html',{"page":page})
 
 def detail(request, id):
-    return render(request,'single.html')
+    article = get_object_or_404(Article, pk = id )
+    return render(request,'single.html', locals())
