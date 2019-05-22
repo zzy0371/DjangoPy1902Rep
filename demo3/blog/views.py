20ed8f32ddc7ceedf54000f13dd506c5531f264c
zzy0372 <18138128152@163.com>
2019-05-22 15:48:16 +0800

    MarkDown

--- demo3/blog/views.py
+++ demo3/blog/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Article
 #  Paginator  Page
 from django.core.paginator import Paginator
+import markdown
 # Create your views here.
 
 def index(request):
@@ -19,4 +20,25 @@ def index(request):
 
 def detail(request, id):
     article = get_object_or_404(Article, pk = id )
+    # 使用markdown处理body  将markdown语法转换为html标签
+
+    # 第一种使用  针对需要处理的article.body 将markdown转为html'
+    # article.body = markdown.markdown(article.body, extensions = [
+    #     "markdown.extensions.extra",
+    #     "markdown.extensions.codehilite",
+    #     "markdown.extensions.toc"
+    # ])
+
+    # 第二种如果在外部使用目录  需要使用构造函数的写法
+    mk = markdown.Markdown(extensions=[
+        "markdown.extensions.extra",
+        "markdown.extensions.codehilite",
+        "markdown.extensions.toc"
+    ] )
+
+    article.body = mk.convert(article.body)
+    # 将markdown 中的目录赋予article的toc对象
+
+    article.toc = mk.toc
+
     return render(request,'single.html', locals())
